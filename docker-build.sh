d4ac4b0e80269b552859cc1e13fa6bdb169b9d2a
Hoàng Ngọc Hạnh <hngochanh@outlook.com>
2019-10-10 14:17:45 +0700

    update

--- docker-build.sh
+++ docker-build.sh
@@ -1,3 +1,3 @@
 sudo git pull
 sudo docker ps -a
-sudo docker-compose -f docker-compose.yml up --detach --scale master=3 --build --remove-orphans --force-recreate
+sudo docker-compose -f docker-compose.yml up --detach --build --remove-orphans --force-recreate
