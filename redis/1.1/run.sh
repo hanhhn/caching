39c303c5023f731c2c32764a88b59efdea39072a
Hoàng Ngọc Hạnh <hngochanh@outlook.com>
2019-10-24 10:24:40 +0700

    update

--- redis/1.1/run.sh
+++ redis/1.1/run.sh
@@ -1,4 +1,10 @@
 sudo git pull
+
+for port in `seq 7001 7006`; do \
+    dir = sudo rm -rf "/app/redis/data-"$hostip
+    echo "remove config "$dir && dir
+done
+
 sudo docker-compose -f docker-compose.yml up -d --build --remove-orphans --force-recreate
 sudo docker ps -a
 sudo bash cluster.sh
