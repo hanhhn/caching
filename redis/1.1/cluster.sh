90111110e327847db5629da50b62fcf9e06cddb7
Hoàng Ngọc Hạnh <hngochanh@outlook.com>
2019-10-24 10:29:12 +0700

    update

--- redis/1.1/cluster.sh
+++ redis/1.1/cluster.sh
@@ -10,7 +10,7 @@ for port in `seq 7001 7006`; do \
     hostip=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "redis-"$port`
     echo "IP for cluster node redis-"$port "is" $hostip
     cluster_hosts="$cluster_hosts$hostip:6379 ";
-    sleep 5
+    sleep 1
 done
 
 echo "cluster hosts "$cluster_hosts
--- redis/1.1/run.sh
+++ redis/1.1/run.sh
@@ -2,6 +2,7 @@ sudo git pull
 
 for port in `seq 7001 7006`; do \
     sudo rm -rf "/app/redis/data-"$hostip
+    echo "remove /app/redis/data-"$hostip
 done
 
 sudo docker-compose -f docker-compose.yml up -d --build
