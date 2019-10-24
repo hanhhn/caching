ef3db108348841d73e6ab13ec18e578eef7e4a6c
Hoàng Ngọc Hạnh <hngochanh@outlook.com>
2019-10-24 09:01:07 +0700

    update

--- redis/1.1/cluster.sh
+++ redis/1.1/cluster.sh
@@ -1,12 +1,15 @@
+
+echo "Implement for Docker version 18.09.7"
+echo "current version" && docker --version
 network_database='net_database'
 #---------- create the cluster ------------------------
 
 cluster_hosts=''
 
 for port in `seq 7001 7006`; do \
- hostip=`docker inspect -f '{{(index .NetworkSettings.Networks "network_database").IPAddress}}' "redis-"$port`;
- echo "IP for cluster node redis-"$port "is" $hostip
- cluster_hosts="$cluster_hosts$hostip:$port ";
+    hostip=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "redis-"$port`
+    echo "IP for cluster node redis-"$port "is" $hostip
+    cluster_hosts="$cluster_hosts$hostip:$port ";
 done
 
 echo "cluster hosts "$cluster_hosts
