4864a4789be1413fca180a8eb1b23961d6aa46e9
Hoàng Ngọc Hạnh <hngochanh@outlook.com>
2019-10-25 15:31:09 +0700

    update

---
+++ redis/demo-1/build.sh
@@ -0,0 +1,13 @@
+for port in `seq 7001 7006`; do \
+    sudo rm -rf "/app/redis/data-"$port
+    echo "remove /app/redis/data-"$port
+done
+
+docker-compose -f docker-compose.yml up -d --build
+
+sleep 3
+
+docker ps -a
+sudo bash cluster.sh
+
+# sudo docker-compose -f docker-compose.yml up -d --build --remove-orphans --force-recreate
