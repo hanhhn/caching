3f78356aa1c3b6a88d085ca482ebd5a645a2ef7f
Hoàng Ngọc Hạnh <hngochanh@outlook.com>
2019-10-25 12:01:22 +0700

    update ok

--- redis/demo/cluster-public-id.sh
+++ redis/demo/cluster-public-id.sh
@@ -7,7 +7,7 @@ network_database='net_database'
 cluster_hosts=''
 
 for port in `seq 7001 7006`; do \
-    hostip=`172.16.50.189:$port`
+    hostip=`172.16.50.189:$port`;
     echo "IP for cluster node redis-"$port "is" $hostip
     cluster_hosts="$cluster_hosts$hostip ";
 done
@@ -16,5 +16,5 @@ sleep 3
 
 echo "cluster hosts "$cluster_hosts
 echo "creating cluster...."
-echo 'yes' | redis-cli --cluster create $cluster_hosts --cluster-replicas 1;
+echo 'yes' |  docker run -i --rm --net host redis:latest redis-cli --cluster create 172.16.50.189:7001 172.16.50.189:7002 172.16.50.189:7003 172.16.50.189:7004 172.16.50.189:7005 172.16.50.189:7006 --cluster-replicas 1;
 
