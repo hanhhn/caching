070855c21497f07c0a390305f985f9b535f825b7
Hoàng Ngọc Hạnh <hngochanh@outlook.com>
2019-10-29 14:13:31 +0700

    update

--- build-api.sh
+++ build-api.sh
@@ -1,11 +1,11 @@
 echo "Build api..."
 sleep 3
 
-docker build -f Dockerfile -t eport-redis-api
-docker build -f nginx/Dockerfile -t eport-redis-nginx
+docker build -t eport-redis-api .
+docker build -t eport-redis-nginx ./nginx
 
 echo "Build eport_redis service"
 sleep 2
 
 docker-compose stack deploy -f docker-compose.yml  eport_redis
-docker-compose stack ls
+docker-compose stack lsdocker
